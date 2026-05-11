ab3174b166c3e48850ef23b23854587b1c106faa
Aleezay123 <aleezanaveed34@gmail.com>
2026-05-11 09:51:45 -0700

    learn kotlin, start, variables and data types, operators and relational, logical operators

--- devaleeza/learnkotlin/src/main/java/com/example/learnkotlin/funMain.kt
+++
@@ -1,5 +0,0 @@
-package com.example.learnkotlin
-
-fun main() {
-    println("Function is printing")
-}
---
+++ devaleeza/learnkotlin/src/main/java/com/example/learnkotlin/operators.kt
@@ -0,0 +1,11 @@
+package com.example.learnkotlin
+
+fun main() {
+    var i = 13
+    var j = 2
+    println( i + j) //15
+    println( i - j) //11
+    println( i * j) //26
+    println( i.toFloat() / j) //6.5
+    println(i % j)
+}
