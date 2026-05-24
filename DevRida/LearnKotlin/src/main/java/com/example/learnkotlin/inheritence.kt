8eb4a4931b22c960c6969a03932b57dd0b967364
RIDA E ZAINAB <zeynip522007@gmail.com>
2026-05-24 09:12:01 -0700

    Inheritence

---
+++ DevRida/LearnKotlin/src/main/java/com/example/learnkotlin/inheritence.kt
@@ -0,0 +1,59 @@
+package com.example.learnkotlin
+//fun main(){
+//
+//    val objchild = child()
+//    objchild.childs()
+//    objchild.parents()
+//    }
+//
+//open class parent{
+//    var name :String= "michal"
+//    var age :Int = 20
+//
+//    fun parents(){
+//        println(" im parent ")
+//    }
+//}
+//
+//class child :parent(){
+//    var name1 :String= "Taliyah"
+//    var age1 :Int = 12
+//
+//    fun childs(){
+//        println("im child")
+//    }
+//}
+//lecture no 20
+//overriding in inheritence
+
+//fun main(){
+//   var oneplus = oneplus()
+//    oneplus.makecalls()
+//    println(oneplus.size)
+//    println(oneplus.name)
+//    oneplus.display()
+//
+//
+//}
+//
+//
+//open class mobile(type :String){
+//   open var name:String ="samsung"
+//   open var size:Int =17
+//    fun makecalls() = println("mobile can make calls")
+//    fun poweroff() = println("mobile can be powered off")
+//    open fun display() = println("simple mobile display")
+//}
+//class oneplus : mobile(type = "general"){
+//    override var name :String = "nokia"
+//    override var size :Int= 20
+//    override fun display() = println("complex mobile display ")
+//}
+//
+//
+
+
+
+
+
+
