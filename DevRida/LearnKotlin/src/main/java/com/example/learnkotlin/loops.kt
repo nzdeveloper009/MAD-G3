953a258e25046ccf4e1d0f2fd06f6b5d0af36e08
RIDA E ZAINAB <zeynip522007@gmail.com>
2026-05-24 09:06:11 -0700

    Loops

---
+++ DevRida/LearnKotlin/src/main/java/com/example/learnkotlin/loops.kt
@@ -0,0 +1,26 @@
+package com.example.learnkotlin
+
+fun main (){
+//    var count =1
+////    while(count >=1){
+////        println("hello")
+////        count--
+//    while(count <=5){
+//        println("hello")
+//        count++
+//    }
+
+//
+//    var num =2
+//    var index =1
+//    while(index <=10){
+//        println(num*index)
+//        index++
+//    }
+//    var num =2
+//    var index =1
+//    do{
+//       println(num  "*" +index + (num*index))
+//       index++
+//    }while(index<=10)
+}
