58a77aaa38f2578cba130a577a5e35a088237ec9
RIDA E ZAINAB <zeynip522007@gmail.com>
2026-05-24 09:09:49 -0700

    Arrays

---
+++ DevRida/LearnKotlin/src/main/java/com/example/learnkotlin/arrays.kt
@@ -0,0 +1,19 @@
+package com.example.learnkotlin
+
+//fun main (){
+//     var arr =arrayOf("one","two","three")
+//    var arr1 = arrayOf(1,2,3)
+//
+//    var arr2 = arrayOf<Int>(4,5,6)
+//
+//    for( (i :Int, e) in arr.withIndex()){
+//        println("$i -$e")///with index
+//        println(arr[0])
+//        println(arr.get(1))
+//        arr.set(0,"HELLO")
+//        println(arr.get(0))
+//        println(arr.size)
+//    }
+//}
+
+
