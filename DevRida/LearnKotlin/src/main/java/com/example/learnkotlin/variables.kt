1fc4159147f4155e0d40f746dad06a6cd4a84a64
RIDA E ZAINAB <zeynip522007@gmail.com>
2026-05-24 09:00:47 -0700

    variable

---
+++ DevRida/LearnKotlin/src/main/java/com/example/learnkotlin/variables.kt
@@ -0,0 +1,37 @@
+package com.example.learnkotlin
+//// lecture 5 variables
+//fun  main(){
+//    var score =5
+//    score = 20
+//    println(score)
+// var tempreature : Double =3.25
+//     val message = " hello"
+//    println(message)
+//    println(tempreature)
+//}
+//
+//
+//
+//
+//// lecture 07  if else statemnets
+//
+//
+//fun main (){
+//   var x =10
+//    var y = 20
+//    if (x>y){
+//        print("x is greter")
+//    }else{
+//        print("x is smallerr")
+//    }
+//
+//    var result = if (x % 2==0) {
+//        "even"
+//    }else {
+//        "odd"
+//    }
+//    print(result)
+//
+//}
+
+
