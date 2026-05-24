35473f8c2f6b441c418652078937f08e913654a5
RIDA E ZAINAB <zeynip522007@gmail.com>
2026-05-24 09:09:08 -0700

    functions2

---
+++ DevRida/LearnKotlin/src/main/java/com/example/learnkotlin/functions2.kt
@@ -0,0 +1,12 @@
+//package com.example.learnkotlin
+//
+//fun main(){
+//    println(add(x = 2,y = 3))
+//    println(add(x= 2.3,y =1.2))
+//}
+//fun add(x :Int,y :Int) :Int{
+//    return x + y
+//}
+//fun add(x :Double ,y:Double):Double{
+//    return x + y
+//}
