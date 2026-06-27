c2116b556a8535cb1a8368bc65fe7f8f3ad0c85d
RIDA E ZAINAB <zeynip522007@gmail.com>
2026-06-27 09:36:12 -0700

    single responsability principle

--- DevRida/LearnKotlin/src/main/java/com/example/learnkotlin/solidprinciples.kt
+++ DevRida/LearnKotlin/src/main/java/com/example/learnkotlin/solidprinciples.kt
@@ -1,17 +1,60 @@
 package com.example.learnkotlin
+class Order(
+    val orderId: Int,
+    val customerName: String,
+    val items: List<String>
+)
 
-//single responsibility principle
-fun main(){
 
-    val order = order(listOf("laptop","Mouse"))
+class OrderCalculator {
 
-    val calculator = ordercalculator()
-    val repository =Orderrepository()
-    val email = Emailservice()
+    fun calculateTotal(): Double {
 
-    calculator.calculatort()
+        val total = 2500.0
+
+        println("Calculating Total Price...")
+        println("Total Price: Rs. $total")
+
+        return total
+    }
+}
+
+
+class OrderRepository {
+
+    fun save(order: Order) {
+
+        println("Saving Order...")
+        println("Order ID: ${order.orderId}")
+        println("Customer: ${order.customerName}")
+    }
+}
+
+
+class EmailService {
+
+    fun sendConfirmation(order: Order) {
+
+        println("Sending Confirmation Email...")
+        println("Email sent to ${order.customerName}")
+    }
+}
+
+
+fun main() {
+
+    val order = Order(
+        101,
+        "Ali",
+        listOf("Laptop", "Mouse")
+    )
+
+    val calculator = OrderCalculator()
+    calculator.calculateTotal()
+
+    val repository = OrderRepository()
     repository.save(order)
-    email.sendconfimation(order)
 
-
+    val emailService = EmailService()
+    emailService.sendConfirmation(order)
 }
